90e5db5615d65341944463256a389f8595bcf275
Chai-YD <1030333788@qq.com>
2018-05-15 23:04:28 -0400

    add HeapSort to Heap

--- Heap.c
+++ Heap.c
@@ -69,7 +69,7 @@ int HeapRoot(Heap* heap, HeapType* value){
         //非法输入
         return 0;
     }
-    //1.判断堆是否为空，空堆返回0，否则返回1
+    //2.判断堆是否为空，空堆返回0，否则返回1
     if(heap->size == 0){
         //空堆 
         return 0;
@@ -78,6 +78,129 @@ int HeapRoot(Heap* heap, HeapType* value){
         *value = heap->data[0];
         return 1;
     }
+}
+//删除堆顶元素
+void HeapErase(Heap* heap){
+    //1.判断是否为非法输入
+    if(heap == NULL){
+        //非法输入
+        return;
+    }
+    //2.判断堆是否为空
+    if(heap->size == 0){
+        //堆为空
+        return;
+    }else{
+        //非空
+        swap(&heap->data[0],&heap->data[heap->size-1]);
+        --heap->size;
+        int cur = 0;
+        int child = 2*cur +1;
+        while(child < heap->size){
+            //当前结点右子树，，
+            //判断该结点是否具有右子树
+            if(child+1<heap->size){
+                //带结点具有右子树
+                //进行左右子树判断，挑选满足规定的值
+                if(heap->cmp(heap->data[child],heap->data[child+1]) == 0){
+                    //child不满足规定
+                     ++child;
+                     if(heap->cmp(heap->data[cur],heap->data[child])== 0){
+                         swap(&heap->data[cur],&heap->data[child]);
+                         cur = child;
+                         child = ( cur-1 ) /2;
+                     }else{
+                         return;
+                     }
+                }else{
+                    //child满足规定
+                     if(heap->cmp(heap->data[cur],heap->data[child])== 0){
+                         swap(&heap->data[cur],&heap->data[child]);
+                         cur = child;
+                         child = 2*cur+1;
+                     }else{
+                         return;
+                     }
+                }
+            }else{
+                //cur无右子树
+                if(heap->cmp(heap->data[cur],heap->data[child])== 0){
+                    swap(&heap->data[cur],&heap->data[child]);
+                    cur = child;
+                    child = ( cur-1 ) /2;
+                }else{
+                    return;
+                }
+            }
+        }
+    }
+    return;
+}
+//堆的销毁
+void HeapDestroy(Heap* heap){
+    //1.判断是否为非法输入
+    if(heap == NULL){
+        //非法输入
+        return;
+    }
+    //2.判断是否为空堆
+    if(heap->size == 0){
+        //为空
+        return;
+    }else{
+        //非空
+        while(heap->size){
+            HeapErase(heap);
+        }
+        return;
+    }
+}
+//堆排序函数
+void HeapSort(HeapType array[], size_t size){
+    int i = 0;
+    Heap heap ;
+    HeapInit(&heap,Greater);
+    for(;i<size;i++){
+        HeapInsert(&heap,array[i]);
+    }
+    HeapDestroy(&heap);
+    Heap new_heap = heap;
+    new_heap.size = size;
+    print_Heap(new_heap,"HeapSord");
+    return;
+}
+
+//堆排序的检测函数
+void textHeapSort(){
+    int array[] = {1,2,3,4};
+    HeapSort(array,sizeof(array)/sizeof(array[0]));
+    return;
+    
+}
+//堆销毁的检测函数
+void textHeapDestroy(){
+    Heap heap ;
+    HeapInit(&heap,Greater);
+    HeapInsert(&heap,1);
+    HeapInsert(&heap,2);
+    HeapInsert(&heap,3);
+    HeapInsert(&heap,4);
+    int size = heap.size;
+    HeapDestroy(&heap);
+    print_Heap(heap,"堆删除");
+    return;
+}
+//删除堆顶元素的检测函数
+void textHeapErase(){
+    Heap heap ;
+    HeapInit(&heap,Greater);
+    HeapInsert(&heap,1);
+    HeapInsert(&heap,2);
+    HeapInsert(&heap,3);
+    HeapInsert(&heap,4);
+    HeapErase(&heap);
+    print_Heap(heap,"删除堆顶元素");
+    return;
 }
 //取堆顶元素的检测函数
 void textHeapRoot(){
@@ -118,5 +241,8 @@ int main(){
     textHeapInit();//堆初始化的检测函数
     textHeapInsert();//堆插入一个元素
     textHeapRoot();//取堆顶元素的检测函数
+    textHeapErase();//删除堆顶元素的检测函数
+    textHeapDestroy();//堆删除的检测函数
+    textHeapSort();//
     return 0;
 }

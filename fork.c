56981b814b528cac45d761998efcc5ebb7f8bd15
Chai-YD <1030333788@qq.com>
2018-06-06 05:22:40 -0400

    add if 分流

--- fork.c
+++ fork.c
@@ -3,7 +3,16 @@
 #include<unistd.h>
 int main(){
     int ret = fork();
-    printf("hello porc:%d!,ret :%d\n",getpid(),ret);
+    if(ret>0){
+        printf("I am father!");
+        printf("hello porc:%d!,ret :%d\n",getpid(),ret);
+    }else if(ret == 0){
+        printf("I am child!");
+        printf("hello porc:%d!,ret :%d\n",getpid(),ret);
+    }else{
+        perror("fork");
+        return 1;
+    }
     sleep(1);
     return 0;
 }

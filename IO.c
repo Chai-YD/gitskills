ccf1d85159cd062cd3056e3f7c6cdef211ae4aa2
Chai-YD <1030333788@qq.com>
2018-06-18 10:27:29 -0400

    add fcntl function to IO

--- IO.c
+++ IO.c
@@ -68,45 +68,81 @@
 //函数fsync的使用
 //**************************************
 
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/file.h>
-
-//主函数
-int main(){
-    int fd, i;
-    char path[] = "/home/chaiyandong/BAT/LINUX/基础IO/txt1.txt";
-    fd = open(path,O_WRONLY|O_CREAT);
-    if(fd != -1){
-        printf("opened file %s\n",path);
-        printf("please input a number to lock the file!\n");//提示输入一个数字
-        scanf("%d",&i);
-        if(flock(fd,LOCK_EX) == 0){//锁定文件
-            printf("the file was locked\n");
-        }else{
-            printf("the file was not locked\n");
-        }
-        printf("please input a number to unlock the file\n");//提示输入一个数字
-        scanf("%d",&i);
-        if(flock(fd,LOCK_UN) == 0){
-            printf("the file was unlocked\n");
-        }else{
-            printf("the file was not unclocked\n");
-        }
-        close(fd);
-    }else{
-        printf("cann't open file %s\n",path);
-    }
-    return 0;
-}
-
-
+//#include<stdio.h>
+//#include<sys/types.h>
+//#include<sys/stat.h>
+//#include<fcntl.h>
+//#include<unistd.h>
+//#include<sys/file.h>
+//
+////主函数
+//int main(){
+//    int fd, i;
+//    char path[] = "/home/chaiyandong/BAT/LINUX/基础IO/txt1.txt";
+//    fd = open(path,O_WRONLY|O_CREAT);
+//    if(fd != -1){
+//        printf("opened file %s\n",path);
+//        printf("please input a number to lock the file!\n");//提示输入一个数字
+//        scanf("%d",&i);
+//        if(flock(fd,LOCK_EX) == 0){//锁定文件
+//            printf("the file was locked\n");
+//        }else{
+//            printf("the file was not locked\n");
+//        }
+//        printf("please input a number to unlock the file\n");//提示输入一个数字
+//        scanf("%d",&i);
+//        if(flock(fd,LOCK_UN) == 0){
+//            printf("the file was unlocked\n");
+//        }else{
+//            printf("the file was not unclocked\n");
+//        }
+//        close(fd);
+//    }else{
+//        printf("cann't open file %s\n",path);
+//    }
+//    return 0;
+//}
 
 
+//**************************************
+//函数fcntl的使用
+//**************************************
 
+//注：文件锁定函数
+//与flock函数不同的是，fcntl可以设定对文件的某一部分进行锁定
+//#include<stdio.h>
+//#include<sys/types.h>
+//#include<sys/stat.h>
+//#include<fcntl.h>
+//#include<unistd.h>
+//
+////主函数
+//int main(){
+//    int fd;
+//    char path[] = "/home/chaiyandong/BAT/LINUX/基础IO/txt3.txt";
+//    struct flock fl;//定义一个flock结构体
+//    char s[] = "hello world";
+//    extern int errno;
+//    fd = open(path,O_WRONLY|O_CREAT);
+//    if(fd != -1){
+//        printf("opened file %s\n",path);
+//    }else{
+//        printf("cann't open file %s\n",path);
+//    }
+//    //构造结构体内容
+//    fl.l_type = F_RDLCK;
+//    fl.l_whence = SEEK_SET;
+//    fl.l_start = 2;
+//    fl.l_len = 10;
+//    fl.l_len = 15;
+//    if(fcntl(fd,F_SETLKW,&fl) == 0){
+//        printf("some string of the file was locked\n");
+//    }else{
+//        printf("not locked\n");
+//    }
+//    close(fd);
+//    return 0;
+//}
 
 
 
